09b2450a7fd8b1970a2444a260e5b620ede20609
kaiIlagan <kayinilagan@gmail.com>
2022-09-21 18:51:22 -0400

    I added gameOver sounds and refactored playSound

--- Homework/Adventure Game/client.js
+++ Homework/Adventure Game/client.js
@@ -1,23 +1,77 @@
 let currentIndex = 0; //what slot of the story we are currently at
 let currentMusic = null;
 
+let text = document.getElementById("text");
 let leftButton = document.getElementById("leftButton");
 let rightButton = document.getElementById("rightButton");
-let text = document.getElementById("text");
+let leftImage = document.getElementById("leftImage");
+let rightImage = document.getElementById("rightImage");
 
 let story = [
     {
-        text: "Press Start", leftChoice: "Start", rightChoice:
-            "Quit", leftIndex: 1, rightIndex: 2, backImage: null
+        //0
+        text: "Press Start to Play",
+        leftChoice: "Start",
+        rightChoice: "Quit",
+        leftImage: "",
+        rightImage: "",
+        leftIndex: 1,
+        rightIndex: 2,
+        backImage: null,
+        soundtrack: "titleLoop.wav",
+        loop: true
     },
     {
-        text: "You are at stairs.  Up or down?", leftChoice: "Up",
-        rightChoice: "Down", leftIndex: 3, rightIndex: 4, backImage:
-            "img/stairs.png"
+        //1
+        text: "You hear an alarm as you slowly wake",
+        leftChoice: "Sleep In",
+        rightChoice: "Turn Off Alarm",
+        leftImage: "",
+        rightImage: "",
+        leftIndex: 3,
+        rightIndex: 4,
+        backImage: null,
+        soundtrack: "alarm.wav",
+        loop: true
     },
     {
-        text: "You see a dragon.", leftChoice: "Fight", rightChoice: "Run sinto the castle", leftIndex: 5, rightIndex: 1, backImage:
-            "img/dragon.png"
+        //2
+        text: "You Lost. You didn't even try!",
+        leftChoice: "Retry",
+        rightChoice: "Start Over",
+        leftImage: "",
+        rightImage: "",
+        leftIndex: 0,
+        rightIndex: 0,
+        backImage: null,
+        soundtrack: "gameOver.wav",
+        loop: false
+    },
+    {
+        //3
+        text: "You sleep in, blissfully dreaming away the day. You don't find a job, you sleepy loser.",
+        leftChoice: "Retry",
+        rightChoice: "Start Over",
+        leftImage: "",
+        rightImage: "",
+        leftIndex: 1,
+        rightIndex: 0,
+        backImage: null,
+        soundtrack: null,
+        loop: false
+    },
+    {
+        //4
+        text: "You Lost. You didn't even try!",
+        leftChoice: "Retry",
+        rightChoice: "",
+        leftImage: "",
+        rightImage: "",
+        leftIndex: 1,
+        rightIndex: 2,
+        backImage: null,
+        soundtrack: null,
+        loop: false
     },
 ];
 
@@ -25,29 +79,51 @@ let story = [
 
 leftButton.addEventListener("click", function () {
     //change the story to the new index based on the left choice click
-    playSound("sounds/select.wav");
+    playSound("select.wav");
     currentIndex = story[currentIndex].leftIndex;
     populate(currentIndex);
 });
 
 rightButton.addEventListener("click", function () {
     //change the story to the new index based on the left choice click
-    playSound("sounds/select.wav");
+    playSound("select.wav");
     currentIndex = story[currentIndex].rightIndex;
     populate(currentIndex);
 });
 
-// Taken from Lab 1 in Dr. Kow's CS365 class
-function playSound(soundUrl) {
-    let mySound = new Audio(soundUrl);
+// Taken from Lab 1 in Dr. Kow's CS365 class and modified for assignment
+function playSound(soundUrl, loop = false) {
+    let mySound = new Audio("sounds/" + soundUrl);
+    if (loop) {
+        mySound.loop = true;
+    }
     mySound.play();
+    return mySound;
+}
+
+function setButtonText(string, element) {
+    if (string == "") {
+        element.style.visibility = "hidden";
+    } else {
+        element.style.visibility = "visible";
+        element.innerText = string;
+    }
 }
 
 function populate(index = 0) {
     text.innerText = story[index].text;
-    leftButton.innerText = story[index].leftChoice;
-    rightButton.innerText = story[index].rightChoice;
-    if (story[index].backImage != null) {
-        document.body.style.backgroundImage = story[index].backImage;
+    setButtonText(story[index].leftChoice, leftButton);
+    setButtonText(story[index].rightChoice, rightButton);
+    if (index == 0) {
+        leftImage.style.display = "none";
+        rightImage.style.display = "none";
+    }
+    if (story[index].soundtrack != null) {
+        if (currentMusic != null) {
+            currentMusic.pause();
+        }
+        currentMusic = playSound(story[index].soundtrack, story[index].loop);
+    } else {
+        currentMusic.pause();
     }
 }
