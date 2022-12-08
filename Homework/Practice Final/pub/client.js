e583cf680cabbb263a899065fd12343f3c1db293
Kayin <kayinilagan@gmail.com>
2022-12-07 20:45:46 -0500

    initial setup of vue in index.html and client.js

---
+++ Homework/Practice Final/pub/client.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+let myApp = Vue.createApp({
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+    mounted() {
+
+    }
+}).mount('#app');
